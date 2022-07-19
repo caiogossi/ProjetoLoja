07023a50ea963a8f9a67b0c0ba5687ea65760a2b
caio_gossi <caio_gossi@usp.br>
2022-07-19 04:54:33 -0300

    Adicionado o básico do site

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Vendedor(models.Model):
+    cpf_vendedor = models.IntegerField(primary_key=True)
+    nome_vendedor = models.CharField(max_length=30, null=False)
+    data_admissao = models.DateField(auto_now=True)
+    salario_bruto = models.DecimalField(max_digits=9, decimal_places=2, null=False)
+    salario_liquido = models.DecimalField(max_digits=9, decimal_places=2, null=False)
+    percentual_comissao = models.DecimalField(max_digits=3, decimal_places=2)
+    quantidade_vendas = models.IntegerField(null=False)
+
+class Venda(models.Model):
+    num_venda = models.IntegerField(primary_key=True,serialize=True)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+
+def render_index(request):
+    return render(request, 'index.html')
+
+def render_nothing(request):
+    return redirect('/index/')
+
+def render_login(request):
+    return render(request, 'login.html')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/index')
+        else:
+            return redirect('/index')
+    else:
+        return redirect('/index')
+
+def submit_logout(request):
+    logout(request)
+    return redirect('/logout_successful/')
+
+def render_logout(request):
+    return render(request, 'logout_successful.html')
